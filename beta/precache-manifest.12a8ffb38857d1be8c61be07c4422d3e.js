a0b16673a93bb84ec57995958d6f0af80f17390f
Andrei Cracanau <andithemudkip@gmail.com>
2019-06-30 18:02:55 +0300

    add beta (react)

---
+++ beta/precache-manifest.12a8ffb38857d1be8c61be07c4422d3e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767cc9c9973020b7a29f19e2c02ce978",
+    "url": "/index.html"
+  },
+  {
+    "revision": "934c0180eb6740f4bc77",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0f84b965616ba6da4f68",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "934c0180eb6740f4bc77",
+    "url": "/static/js/2.9cadbc46.chunk.js"
+  },
+  {
+    "revision": "0f84b965616ba6da4f68",
+    "url": "/static/js/main.f58a0bc9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "976fa721fd381554cc6bf898197053fa",
+    "url": "/static/media/AboutMe.976fa721.md"
+  },
+  {
+    "revision": "1df65a2ffd3bf6387a2d8e7072ba58eb",
+    "url": "/static/media/BotUtil.1df65a2f.md"
+  },
+  {
+    "revision": "090ba8df7e07a9299c3da0a7e8e2b621",
+    "url": "/static/media/DeployBotsGuide.090ba8df.md"
+  },
+  {
+    "revision": "921ccfe4abc6e9e3a4fa65ca6a38270c",
+    "url": "/static/media/FacebookBotsGuide.921ccfe4.md"
+  },
+  {
+    "revision": "1e3f96b4ca3ee0b90a2875448db8ddcc",
+    "url": "/static/media/NodeP5.1e3f96b4.md"
+  }
+]);
---
+++ beta/static/media/AboutMe.976fa721.md
@@ -0,0 +1,7 @@
+# Hello, I'm Andi! 😉
+
+I like to think I'm a creative person, so I thought I'd make a site where I can show off everything I'm working on / have worked on.
+
+I like to dabble in all sorts of technologies, mainly web dev stuff - I am making this site as an exercise to learn React haha.
+
+I have a bunch of ongoing projects, including my [Facebook Bots](#), my Node modules, and my [game](https://sphynxgames.com);
